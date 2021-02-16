24c7e64c4315c68d9a715fd2db8ab7de6d35af76
Pete Batard <pete@akeo.ie>
2021-02-16 11:50:33 +0000

    Fixed the use of #warning for MSVC
    #warning is a GCC-ism that is not understood by MSVC.
    Use #pragma message() instead for this compiler.

--- libntfs-3g/ioctl.c
+++ libntfs-3g/ioctl.c
@@ -412,6 +412,8 @@ int ntfs_ioctl(ntfs_inode *ni, unsigned long cmd,
 			range->len = trimmed;
 		}
 		break;
+#elif defined(_MSC_VER)
+#pragma message("Warning: Trimming not supported : FITRIM or BLKDISCARD not defined")
 #else
 #warning Trimming not supported : FITRIM or BLKDISCARD not defined
 #endif
