fd4a8d9341c3bfbb28f28c4bcf3b2755f00cb9fc
Pete Batard <pete@akeo.ie>
2021-02-23 02:43:36 +0000

    Added UEFI compatible string function calls

--- libntfs-3g/uefi_compat.c
+++ libntfs-3g/uefi_compat.c
@@ -35,6 +35,7 @@
 #include <Uefi.h>
 #include <Library/BaseLib.h>
 #include <Library/UefiLib.h>
+#include <Library/PrintLib.h>
 #include <Library/BaseMemoryLib.h>
 #include <Library/MemoryAllocationLib.h>
 #include <Library/UefiRuntimeServicesTableLib.h>
@@ -133,6 +134,254 @@ int memcmp(const void* s1, const void* s2, size_t n)
 	return (int)CompareMem(s1, s2, n);
 }
 #endif /* USE_COMPILER_INTRINSICS_LIB */
+
+/*
+ * Secure string length, that asserts if the string is NULL or if
+ * the length is larger than a predetermined value (STRING_MAX)
+ */
+size_t strlen(const char* s)
+{
+	size_t r;
+
+	FS_ASSERT(s != NULL);
+#ifdef __MAKEWITH_GNUEFI
+	r = strlena(s);
+#else
+	r = AsciiStrnLenS(s, STRING_MAX);
+#endif
+	FS_ASSERT(r < STRING_MAX);
+	return r;
+}
+
+int strcmp(const char* s1, const char* s2)
+{
+#ifdef __MAKEWITH_GNUEFI
+	return (int)strcmpa(s1, s2);
+#else
+	return (int)AsciiStrCmp(s1, s2);
+#endif
+}
+
+int strncmp(const char* s1, const char* s2, size_t n)
+{
+#ifdef __MAKEWITH_GNUEFI
+	return (int)strncmpa(s1, s2, n);
+#else
+	return (int)AsciiStrnCmp(s1, s2, n);
+#endif
+}
+
+char* strcpy(char* dst, const char* src)
+{
+	char* ret;
+	FS_ASSERT(dst != NULL);
+	/* strlen validates the sanity of the source */
+	ret = memcpy(dst, src, strlen(src) + 1);
+	dst[strlen(src)] = 0;
+	return ret;
+}
+
+char* strncpy(char* dst, const char* src, size_t n)
+{
+	FS_ASSERT(dst != NULL);
+	/* strlen validates the sanity of the source */
+	return memcpy(dst, src, MIN(strlen(src) + 1, n));
+}
+
+char* strcat(char* dst, const char* src)
+{
+	FS_ASSERT(dst != NULL);
+	/* strcpy validates the sanity of src and dst */
+	return strcpy(&dst[strlen(dst)], src);
+}
+
+char* strdup(const char* s)
+{
+	FS_ASSERT(s != NULL);
+	char* ret = malloc(strlen(s) + 1);
+	if (ret == NULL)
+		return NULL;
+	return memcpy(ret, s, strlen(s) + 1);
+}
+
+int snprintf(char* str, size_t size, const char* format, ...)
+{
+	size_t i, ret;
+	VA_LIST args;
+	char* ascii_format;
+
+	if (!str || !format) {
+		errno = EINVAL;
+		return -1;
+	}
+
+	ascii_format = strdup(format);
+	if (ascii_format == NULL) {
+		/* To silence an MSVC warning */
+		str[0] = 0;
+		errno = ENOMEM;
+		return -1;
+	}
+
+	/* Convert %s sequences to %a */
+	for (i = 1; i < strlen(format); i++)
+		if ((ascii_format[i] == 's') && (ascii_format[i - 1] == '%'))
+			ascii_format[i] = 'a';
+
+	VA_START(args, format);
+	ret = AsciiVSPrint(str, size, ascii_format, args);
+	VA_END(args);
+
+	free(ascii_format);
+	return (int)ret;
+}
+
+char* strchr(const char* s, int c)
+{
+	FS_ASSERT(strlen(s) < STRING_MAX);
+	do {
+		if (*s == c)
+			return (char*)s;
+	} while (*s++);
+
+	return NULL;
+}
+
+char* strrchr(const char* s, int c)
+{
+	char* p = NULL;
+	FS_ASSERT(strlen(s) < STRING_MAX);
+	do {
+		if (*s == c)
+			p = (char*)s;
+	} while (*s++);
+
+	return p;
+}
+
+char* strstr(const char* s1, const char* s2)
+{
+	const char* p1 = s1;
+	const char* p2 = s2;
+	const char* r;
+	
+	FS_ASSERT(strlen(s1) < STRING_MAX);
+	FS_ASSERT(strlen(s2) < STRING_MAX);
+	r = (*p2 == 0) ? s1 : 0;
+
+	while (*p1 != 0 && *p2 != 0) {
+		if (*p1 == *p2) {
+			if (r == 0)
+				r = p1;
+			p2++;
+		} else {
+			p2 = s2;
+			if (r != 0)
+				p1 = r + 1;
+			if (*p1 == *p2) {
+				r = p1;
+				p2++;
+			} else {
+				r = 0;
+			}
+		}
+		p1++;
+	}
+
+	return (*p2 == 0) ? (char*)r : NULL;
+}
+
+char* strerror(int errnum)
+{
+	static char default_message[] = "Unknown error code 000";
+
+	/* Mostly taken from ReactOS */
+	switch (errnum) {
+	case 0:
+		return "No Error";
+	case EPERM:
+		return "Operation not permitted (EPERM)";
+	case ENOENT:
+		return "No such file or directory (ENOENT)";
+	case ESRCH:
+		return "No such process (ESRCH)";
+	case EINTR:
+		return "Interrupted system call (EINTR)";
+	case EIO:
+		return "Input or output error (EIO)";
+	case ENXIO:
+		return "No such device or address (ENXIO)";
+	case E2BIG:
+		return "Argument list too long (E2BIG)";
+	case ENOEXEC:
+		return "Unable to execute file (ENOEXEC)";
+	case EBADF:
+		return "Bad file descriptor (EBADF)";
+	case ECHILD:
+		return "No child processes (ECHILD)";
+	case EAGAIN:
+		return "Resource temporarily unavailable (EAGAIN)";
+	case ENOMEM:
+		return "Not enough memory (ENOMEM)";
+	case EACCES:
+		return "Permission denied (EACCES)";
+	case EFAULT:
+		return "Bad address (EFAULT)";
+	case EBUSY:
+		return "Resource busy (EBUSY)";
+	case EEXIST:
+		return "File exists (EEXIST)";
+	case EXDEV:
+		return "Improper link (EXDEV)";
+	case ENODEV:
+		return "No such device (ENODEV)";
+	case ENOTDIR:
+		return "Not a directory (ENOTDIR)";
+	case EISDIR:
+		return "Is a directory (EISDIR)";
+	case EINVAL:
+		return "Invalid argument (EINVAL)";
+	case ENFILE:
+	case EMFILE:
+		return "Too many open files (ENFILE/EMFILE)";
+	case ENOTTY:
+		return "Inappropriate I/O control operation (ENOTTY)";
+	case EFBIG:
+		return "File too large (EFBIG)";
+	case ENOSPC:
+		return "No space left on drive (ENOSPC)";
+	case ESPIPE:
+		return "Invalid seek (ESPIPE)";
+	case EROFS:
+		return "Read-only file system (EROFS)";
+	case EMLINK:
+		return "Too many links (EMLINK)";
+	case EPIPE:
+		return "Broken pipe (EPIPE)";
+	case EDOM:
+	case ERANGE:
+		return "Input or output to function out of range (EDOM/ERANGE)";
+	case EDEADLK:
+		return "Resource deadlock (EDEADLK)";
+	case ENAMETOOLONG:
+		return "File name too long (ENAMETOOLONG)";
+	case ENOLCK:
+		return "No locks available (ENOLCK)";
+	case ENOSYS:
+		return "Function not implemented (ENOSYS)";
+	case ENOTEMPTY:
+		return "Directory not empty (ENOTEMPTY)";
+	case EILSEQ:
+		return "Illegal byte sequence (EILSEQ)";
+	default:
+		errnum &= 0xff;
+		default_message[19] = (errnum / 100) + '0';
+		errnum %= 100;
+		default_message[20] = (errnum / 10) + '0';
+		default_message[21] = (errnum % 10) + '0';
+		return default_message;
+	}
+}
 
 /*
  * Returns the current time in a timespec struct.
