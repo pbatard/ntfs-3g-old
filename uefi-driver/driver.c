192a7d076ab4437ed35a38f4819f3d69d02efab5
Pete Batard <pete@akeo.ie>
2021-02-23 17:08:36 +0000

    Fleshed out UEFI driver's FSDriverInstall/FSDriverUninstall
    Based on similar code from EfiFs: https://github.com/pbatard/efifs.
    
    It should be noted that the *only* code we are importing from EfiFs
    is the one that is compliant with GPLv2+.

--- uefi-driver/driver.c
+++ uefi-driver/driver.c
@@ -1,6 +1,8 @@
 /* driver.c - ntfs-3g UEFI filesystem driver */
 /*
- *  Copyright © 2021 Pete Batard <pete@akeo.ie>
+ *  Copyright © 2014-2021 Pete Batard <pete@akeo.ie>
+ *  Based on iPXE's efi_driver.c and efi_file.c:
+ *  Copyright © 2011,2013 Michael Brown <mbrown@fensystems.co.uk>
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -17,11 +19,43 @@
  */
 
 #include "driver.h"
+#include "uefi_logging.h"
 
 #ifdef __MAKEWITH_GNUEFI
 /* Designate the driver entrypoint */
 EFI_DRIVER_ENTRY_POINT(FSDriverInstall)
 #endif /* __MAKEWITH_GNUEFI */
+
+/* We'll try to instantiate a custom protocol as a mutex, so we need a GUID */
+EFI_GUID MutexGUID = NTFS_MUTEX_GUID;
+
+/* Keep a global copy of our ImageHanle */
+EFI_HANDLE EfiImageHandle = NULL;
+
+/* Handle for our custom protocol/mutex instance */
+static EFI_HANDLE MutexHandle = NULL;
+
+/* Keep track of the mounted filesystems */
+LIST_ENTRY FsListHead;
+
+/* Custom protocol/mutex definition */
+typedef struct {
+	INTN Unused;
+} EFI_MUTEX_PROTOCOL;
+static EFI_MUTEX_PROTOCOL MutexProtocol = { 0 };
+
+static EFI_DRIVER_BINDING_PROTOCOL FSDriverBinding = {
+	/* This field is used by the EFI boot service ConnectController() to determine the order
+	 * that driver's Supported() service will be used when a controller needs to be started.
+	 * EFI Driver Binding Protocol instances with higher Version values will be used before
+	 * ones with lower Version values. The Version values of 0x0-0x0f and
+	 * 0xfffffff0-0xffffffff are reserved for platform/OEM specific drivers. The Version
+	 * values of 0x10-0xffffffef are reserved for IHV-developed drivers.
+	 */
+	.Version = 0x10,
+	.ImageHandle = NULL,
+	.DriverBindingHandle = NULL
+};
 
 /**
  * Uninstall EFI driver
@@ -32,7 +66,39 @@ EFI_DRIVER_ENTRY_POINT(FSDriverInstall)
 EFI_STATUS EFIAPI
 FSDriverUninstall(EFI_HANDLE ImageHandle)
 {
-	Print(L"NTFS driver uninstalled.\n");
+	EFI_STATUS Status;
+	UINTN NumHandles;
+	EFI_HANDLE* Handles = NULL;
+	UINTN i;
+
+	/* Enumerate all handles */
+	Status = gBS->LocateHandleBuffer(AllHandles, NULL, NULL, &NumHandles, &Handles);
+
+	/* Disconnect controllers linked to our driver. This action will trigger a call to BindingStop */
+	if (Status == EFI_SUCCESS) {
+		for (i = 0; i < NumHandles; i++) {
+			/* Make sure to filter on DriverBindingHandle, else EVERYTHING gets disconnected! */
+			Status = gBS->DisconnectController(Handles[i], FSDriverBinding.DriverBindingHandle, NULL);
+			if (Status == EFI_SUCCESS)
+				PrintDebug(L"DisconnectController[%d]\n", i);
+		}
+	} else {
+		PrintStatusError(Status, L"Unable to enumerate handles");
+	}
+	if (Handles != NULL)
+		gBS->FreePool(Handles);
+
+	/* Now that all controllers are disconnected, we can safely remove our protocols */
+	gBS->UninstallMultipleProtocolInterfaces(ImageHandle,
+		&gEfiDriverBindingProtocolGuid, &FSDriverBinding,
+		NULL);
+
+	/* Uninstall our mutex (we're the only instance that can run this code) */
+	gBS->UninstallMultipleProtocolInterfaces(MutexHandle,
+		&MutexGUID, &MutexProtocol,
+		NULL);
+
+	PrintDebug(L"FS driver uninstalled.\n");
 	return EFI_SUCCESS;
 }
 
@@ -46,9 +112,64 @@ FSDriverUninstall(EFI_HANDLE ImageHandle)
 EFI_STATUS EFIAPI
 FSDriverInstall(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable)
 {
+	EFI_STATUS Status;
+	EFI_LOADED_IMAGE_PROTOCOL* LoadedImage = NULL;
+	VOID* Interface;
+
 #ifdef __MAKEWITH_GNUEFI
 	InitializeLib(ImageHandle, SystemTable);
 #endif
-	Print(L"NTFS driver installed.\n");
+	SetLogging();
+	EfiImageHandle = ImageHandle;
+
+	/* Prevent the driver from being loaded twice by detecting and trying to
+	 * instantiate a custom protocol, which we use as a global mutex.
+	 */
+	Status = gBS->LocateProtocol(&MutexGUID, NULL, &Interface);
+	if (Status == EFI_SUCCESS) {
+		PrintError(L"This driver has already been installed\n");
+		return EFI_LOAD_ERROR;
+	}
+	/* The only valid status we expect is NOT FOUND here */
+	if (Status != EFI_NOT_FOUND) {
+		PrintStatusError(Status, L"Could not locate global mutex");
+		return Status;
+	}
+	Status = gBS->InstallMultipleProtocolInterfaces(&MutexHandle,
+		&MutexGUID, &MutexProtocol,
+		NULL);
+	if (EFI_ERROR(Status)) {
+		PrintStatusError(Status, L"Could not install global mutex");
+		return Status;
+	}
+
+	/* Grab a handle to this image, so that we can add an unload to our driver */
+	Status = gBS->OpenProtocol(ImageHandle, &gEfiLoadedImageProtocolGuid,
+		(VOID**)&LoadedImage, ImageHandle,
+		NULL, EFI_OPEN_PROTOCOL_GET_PROTOCOL);
+	if (EFI_ERROR(Status)) {
+		PrintStatusError(Status, L"Could not open loaded image protocol");
+		return Status;
+	}
+
+	/* Configure driver binding protocol */
+	FSDriverBinding.ImageHandle = ImageHandle;
+	FSDriverBinding.DriverBindingHandle = ImageHandle;
+
+	/* Install driver */
+	Status = gBS->InstallMultipleProtocolInterfaces(&FSDriverBinding.DriverBindingHandle,
+		&gEfiDriverBindingProtocolGuid, &FSDriverBinding,
+		NULL);
+	if (EFI_ERROR(Status)) {
+		PrintStatusError(Status, L"Could not bind driver");
+		return Status;
+	}
+
+	/* Register the uninstall callback */
+	LoadedImage->Unload = FSDriverUninstall;
+
+	InitializeListHead(&FsListHead);
+
+	PrintDebug(L"FS driver installed.\n");
 	return EFI_SUCCESS;
 }
